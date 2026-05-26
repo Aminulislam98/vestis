7a5c55cc6e6a97ee9fb9cabb30195f6f4489fa09
Aminul islam <aminurrohman98@gmail.com>
2026-05-26 17:49:33 +0100

    made 2 group main and auth

---
+++ src/app/(main)/layout.js
@@ -0,0 +1,12 @@
+import Footer from "@/components/layout/Footer/Footer";
+import Navbar from "@/components/layout/Navbar/Navbar";
+
+export default function MainLayout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
