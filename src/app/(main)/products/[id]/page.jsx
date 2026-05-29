031e30c1cf738d35c850f755628fd2290b4b9fca
Aminul islam <aminurrohman98@gmail.com>
2026-05-29 23:51:03 +0100

    deploy client side in vercel

--- src/app/(main)/products/[id]/page.jsx
+++ src/app/(main)/products/[id]/page.jsx
@@ -1,10 +1,22 @@
 import ProductDetailPageClient from "@/components/product/ProductDetailPageClient";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import React from "react";
 
 const ProductDetailPage = async ({ params }) => {
   const { id } = await params;
+  const token = await auth.api.getToken({
+    headers: await headers(),
+  });
 
-  const res = await fetch(`http://localhost:4000/product/${id}`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/product/${id}`,
+    {
+      headers: {
+        authorization: `Bearer ${token.token}`,
+      },
+    },
+  );
   const product = await res.json();
 
   return (
@@ -15,3 +27,4 @@ const ProductDetailPage = async ({ params }) => {
 };
 
 export default ProductDetailPage;
+// const { data: token } = await authClient.token();
